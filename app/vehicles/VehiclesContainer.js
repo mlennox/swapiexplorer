e9889457bfbfefefd088115e553409b9adeec8a1
mlennox <mark@webpusher.ie>
2017-01-22 04:20:11 +0000

    added vehicles

--- app/vehicles/VehiclesContainer.js
+++ app/vehicles/VehiclesContainer.js
@@ -1,38 +1,18 @@
 import React, { Component } from 'react'
-import DataContainerBase from '../containers/DataContainerBase'
-import Starships from './index'
+import DC_HOC from '../containers/DataContainerHOC'
+import Vehicles from './index'
 
+class VehiclesContainer extends Component {
 
-export default class StarshipsContainer extends DataContainerBase {
-
-  constructor() {
-    super()
-    this.state = {
-      data: {}
-    }
-    this.fetch_details = {
-      url: 'https://swapi.co/api/vehicles/',
-      options: { method: 'get' }
-    }
-  }
-
-  results_parser = response => {
-    try {
-      return JSON.parse(response._bodyText).results
-    }
-    catch (err) {
-      console.log('results parser error - - - - - - - - - - -', err)
-    }
-  }
-
-  results_assigner = results => {
-    this.setState({
-      data: results
-    })
+  fetch_details = {
+    url: 'https://swapi.co/api/vehicles/',
+    options: { method: 'get' }
   }
 
   render() {
-    return <Starships data={this.state.data} navigate={this.props.navigate} />
+    return <Vehicles data={this.state.data} navigate={this.props.navigate} />
   }
 
 }
+
+export default DC_HOC(VehiclesContainer)
