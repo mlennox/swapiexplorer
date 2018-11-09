9d5375f1f8fc525c3c594d8c2e04b37aace6a57d
Mark Lennox <mark@webpusher.ie>
2018-11-09 08:47:11 +0000

    very basic app with routes, links and stateless components for each page all in the one file

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const Home = () => <h1>Home</h1>;
+const People = () => <h1>People</h1>;
+const Planets = () => <h1>Planets</h1>;
+const Species = () => <h1>Species</h1>;
+const Ships = () => <h1>Ships</h1>;
+const Vehicles = () => <h1>Vehicles</h1>;
+const Films = () => <h1>Films</h1>;
+
+const Links = () => (
+  <nav>
+    <Link to="/">Home</Link>
+    <Link to="/people">People</Link>
+    <Link to="/planets">Planets</Link>
+    <Link to="/species">Species</Link>
+    <Link to="/ships">Ships</Link>
+    <Link to="/vehicles">Vehicles</Link>
+    <Link to="/films">Films</Link>
+  </nav>
+);
+
+const App = () => (
+  <Router>
+    <Links />
+    <Route exact={true} path="/" component={Home} />
+    <Route path="/people" component={People} />
+    <Route path="/planets" component={Planets} />
+    <Route path="/species" component={Species} />
+    <Route path="/ships" component={Ships} />
+    <Route path="/vehicles" component={Vehicles} />
+    <Route path="/films" component={Films} />
+  </Router>
+);
 
 export default App;
