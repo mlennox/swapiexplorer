8f1c734b227a0f3afc185791c10deabb25abaab1
Mark Lennox <mark@webpusher.ie>
2018-11-11 22:36:12 +0000

    checking snapshot in App test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from "react";
-import Nav from "./nav/";
+import Nav from "./nav/nav";
 
 const App = () => (
   <Fragment>
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,13 @@
 import React from "react";
 import App from "./App";
-import { render } from "react-testing-library";
+import { render, cleanup } from "react-testing-library";
+import "jest-dom/extend-expect";
+
+afterEach(cleanup);
 
 describe("App", () => {
   test("it renders without crashing", () => {
-    render(<App />);
+    const { container } = render(<App />);
+    expect(container.firstChild).toMatchSnapshot();
   });
 });
