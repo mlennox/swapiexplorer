ce9ddcc9507bb7ce909d0e3f9086690ee3af77d1
Mark Lennox <mark@webpusher.ie>
2018-11-11 21:44:33 +0000

    updating App to mmove router and links to nav

--- src/App.js
+++ src/App.js
@@ -1,37 +1,10 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import Films from "./films";
-import People from "./people";
-import Planets from "./planets";
-import Species from "./species";
-import Ships from "./ships";
-import Vehicles from "./vehicles";
-
-const Home = () => <h1>Home</h1>;
-
-const Links = () => (
-  <nav>
-    <Link to="/">Home</Link>
-    <Link to="/people">People</Link>
-    <Link to="/planets">Planets</Link>
-    <Link to="/species">Species</Link>
-    <Link to="/ships">Ships</Link>
-    <Link to="/vehicles">Vehicles</Link>
-    <Link to="/films">Films</Link>
-  </nav>
-);
+import React, { Fragment } from "react";
+import Nav from "./nav/";
 
 const App = () => (
-  <Router>
-    <Links />
-    <Route exact={true} path="/" component={Home} />
-    <Route path="/people" component={People} />
-    <Route path="/planets" component={Planets} />
-    <Route path="/species" component={Species} />
-    <Route path="/ships" component={Ships} />
-    <Route path="/vehicles" component={Vehicles} />
-    <Route path="/films" component={Films} />
-  </Router>
+  <Fragment>
+    <Nav />
+  </Fragment>
 );
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import App from "./App";
+import { render } from "react-testing-library";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("App", () => {
+  test("it renders without crashing", () => {
+    render(<App />);
+  });
 });
